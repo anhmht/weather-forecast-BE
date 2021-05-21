c8ef8adfb15f7d33adcb3e68b7d2f968cc63ab54
phuonglt <phuonglt@omnicasa.com>
2021-05-21 05:45:04 +0200

    test git

--- GloboWeather.WeatherManagement.Persistence/PersistenceServiceRegistration.cs
+++ GloboWeather.WeatherManagement.Persistence/PersistenceServiceRegistration.cs
@@ -15,6 +15,7 @@ namespace GloboWeather.WeatherManagement.Persistence
                 options.UseSqlServer(
                     configuration.GetConnectionString("GloboWeatherWeatherManagementConnectionString")));
             services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IEventRepository, EventRepository>();
             
             return services;
         }
